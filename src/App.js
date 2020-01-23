b3739b8c6f0cead97bda59c7bb07d6de71e43dfe
hzuber-vayyar <59413508+hzuber-vayyar@users.noreply.github.com>
2020-01-23 17:29:50 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,61 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      name: "",
+      position: "",
+      phone: "",
+      submitted: false
+    }
+  }
+
+  handleChange=(event)=>{
+    this.setState({
+      [event.target.id] : event.target.value
+    })
+  }
+  
+  handleSubmit=(event)=> {
+    event.preventDefault();
+    this.setState({submitted:true})
+  }
+
+  render(){
+    const length = this.state.name.length
+    const weight = length < 20 ? "weight-regular" :  "lighter"
+    const size = length < 30 ? "font-regular" : length < 40 ? "smaller" : "smallest"
+    const showCard = this.state.submitted ? "display" : "display-none"
+    return (
+      <div className="App">
+        <header className="App-header">
+          Explanation here
+        </header>
+        <form onSubmit={(e)=>this.handleSubmit(e)}>
+          <label>
+            Name:
+            <input type="text" id="name" value={this.state.name} onChange={(e) => this.handleChange(e)}/>
+          </label>
+          <label>
+            Position:
+            <input type="text" id="position" value={this.state.position} onChange={(e) => this.handleChange(e)}/>
+          </label>
+          <label>
+            Phone Number:
+            <input type="text" id="phone" value={this.state.number} onChange={(e) => this.handleChange(e)}/>
+          </label>
+          <input type="submit" value="Submit" className="submit-btn"/>
+        </form>
+        <div className={`${showCard} sigature-card`}>
+          <div className={`display-name ${weight} ${size}`}>{this.state.name}</div>
+          <div className="display-position">{this.state.position}</div>
+          <div className="display-number">{this.state.phone}</div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
